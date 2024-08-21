a43f226652ead758dfab172e44239280062d94cd
xfdzcoder <xfdzcoder@gmail.com>
2024-08-21 21:21:28 +0800

    feat: 引入 Nacos 配置中心和服务发现

--- code-sandbox/src/main/java/io/github/xfdzcoder/noj/cloud/sandbox/CodeSandboxApplication.java
+++ code-sandbox/src/main/java/io/github/xfdzcoder/noj/cloud/sandbox/CodeSandboxApplication.java
@@ -2,11 +2,15 @@ package io.github.xfdzcoder.noj.cloud.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author: xfdzcoder
  */
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class CodeSandboxApplication {
 
