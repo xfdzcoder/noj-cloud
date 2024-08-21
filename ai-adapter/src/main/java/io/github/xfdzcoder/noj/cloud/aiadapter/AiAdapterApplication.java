a3241c73bebd183597123a2ffc33d8f6c03ab513
xfdzcoder <xfdzcoder@gmail.com>
2024-08-21 21:51:13 +0800

    feat: 新增 ai-adapter 模块

---
+++ ai-adapter/src/main/java/io/github/xfdzcoder/noj/cloud/aiadapter/AiAdapterApplication.java
@@ -0,0 +1,17 @@
+package io.github.xfdzcoder.noj.cloud.aiadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xfdzcoder
+ */
+
+@SpringBootApplication
+public class AiAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAdapterApplication.class, args);
+    }
+
+}
