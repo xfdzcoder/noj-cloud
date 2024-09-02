aabef2190375b33758ef18452e0b235b83403928
xfdzcoder <xfdzcoder@gmail.com>
2024-09-02 09:15:39 +0800

    feat: 完成登出功能

--- manage/manage-user/src/main/java/io/github/xfdzcoder/noj/cloud/manage/user/controller/AuthController.java
+++ manage/manage-user/src/main/java/io/github/xfdzcoder/noj/cloud/manage/user/controller/AuthController.java
@@ -1,15 +1,14 @@
 package io.github.xfdzcoder.noj.cloud.manage.user.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import io.github.xfdzcoder.noj.cloud.common.web.pojo.Response;
+import io.github.xfdzcoder.noj.cloud.manage.common.dependencies.consts.AuthConst;
 import io.github.xfdzcoder.noj.cloud.manage.user.dto.req.LoginReq;
 import io.github.xfdzcoder.noj.cloud.manage.user.dto.resp.LoginResp;
 import io.github.xfdzcoder.noj.cloud.manage.user.exception.ManageUserException;
 import io.github.xfdzcoder.noj.cloud.manage.user.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author xfdzcoder
@@ -28,6 +27,12 @@ public class AuthController {
             throw new ManageUserException("请先进行人机验证");
         }
         return Response.ok(authService.login(loginReq));
+    }
+
+    @GetMapping("/logout")
+    public Response<String> logout() {
+        StpUtil.logout();
+        return Response.ok();
     }
 
 }
