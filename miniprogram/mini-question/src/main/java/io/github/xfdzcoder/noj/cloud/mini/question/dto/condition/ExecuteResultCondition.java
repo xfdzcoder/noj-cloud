281a307b3cfc7cb1497438e6952b0dec0a078f87
xfdzcoder <xfdzcoder@gmail.com>
2024-09-10 17:51:44 +0800

    fix: 修改执行结果表默认查询排序规则

--- miniprogram/mini-question/src/main/java/io/github/xfdzcoder/noj/cloud/mini/question/dto/condition/ExecuteResultCondition.java
+++ miniprogram/mini-question/src/main/java/io/github/xfdzcoder/noj/cloud/mini/question/dto/condition/ExecuteResultCondition.java
@@ -1,5 +1,6 @@
 package io.github.xfdzcoder.noj.cloud.mini.question.dto.condition;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.github.xfdzcoder.noj.cloud.common.dao.dto.condition.BaseCondition;
 import io.github.xfdzcoder.noj.cloud.mini.question.entity.ExecuteResult;
 import lombok.EqualsAndHashCode;
@@ -13,4 +14,9 @@ import lombok.Data;
 @EqualsAndHashCode(callSuper = true)
 public class ExecuteResultCondition extends BaseCondition<ExecuteResult> {
 
+    @Override
+    public LambdaQueryWrapper<ExecuteResult> getLambdaQueryWrapper() {
+        return super.getLambdaQueryWrapper()
+                .orderByDesc(ExecuteResult::getCreateDateTime);
+    }
 }
