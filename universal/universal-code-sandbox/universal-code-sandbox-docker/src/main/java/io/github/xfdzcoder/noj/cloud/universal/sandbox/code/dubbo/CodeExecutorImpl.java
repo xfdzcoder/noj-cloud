ddb1bb8c489a8c2434d50d4e798db2a430c3f169
xfdzcoder <xfdzcoder@gmail.com>
2024-09-15 22:48:29 +0800

    fix: 新增对测试用例为空的判断

--- universal/universal-code-sandbox/universal-code-sandbox-docker/src/main/java/io/github/xfdzcoder/noj/cloud/universal/sandbox/code/dubbo/CodeExecutorImpl.java
+++ universal/universal-code-sandbox/universal-code-sandbox-docker/src/main/java/io/github/xfdzcoder/noj/cloud/universal/sandbox/code/dubbo/CodeExecutorImpl.java
@@ -1,5 +1,7 @@
 package io.github.xfdzcoder.noj.cloud.universal.sandbox.code.dubbo;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.StrFormatter;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.entity.TestCase;
 import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.CodeExecutor;
@@ -7,6 +9,7 @@ import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.DockerJavaEx
 import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.TestCaseService;
 import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.dto.ExecuteReq;
 import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.dto.ExecuteResp;
+import io.github.xfdzcoder.noj.cloud.universal.sandbox.code.service.dto.ExitTypeEnum;
 import jakarta.annotation.PostConstruct;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,6 +57,13 @@ public class CodeExecutorImpl implements CodeExecutor {
     public ExecuteResp executeSync(final ExecuteReq executeReq) {
         List<TestCase> caseList = testCaseService.list(new LambdaQueryWrapper<TestCase>()
                 .eq(TestCase::getQuestionInfoId, executeReq.getQuestionInfoId()));
+
+        if (CollUtil.isEmpty(caseList)) {
+            ExecuteResp resp = new ExecuteResp();
+            resp.setExitType(ExitTypeEnum.SYSTEM_ERROR);
+            resp.setThrowableOutput(StrFormatter.format("ID为 {} 的题目的可用测试用例为空", executeReq.getQuestionInfoId()));
+            return resp;
+        }
 
         return dockerJavaExecutor.execute(executeReq, caseList);
     }
