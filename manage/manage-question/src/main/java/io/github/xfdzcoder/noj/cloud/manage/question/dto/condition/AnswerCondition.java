243107f1c18650c0c99430e909516acd901ddc87
xfdzcoder <xfdzcoder@gmail.com>
2024-09-17 11:22:18 +0800

    fix: 修复答案查询没有根据题目信息查的bug

--- manage/manage-question/src/main/java/io/github/xfdzcoder/noj/cloud/manage/question/dto/condition/AnswerCondition.java
+++ manage/manage-question/src/main/java/io/github/xfdzcoder/noj/cloud/manage/question/dto/condition/AnswerCondition.java
@@ -1,14 +1,24 @@
 package io.github.xfdzcoder.noj.cloud.manage.question.dto.condition;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.github.xfdzcoder.noj.cloud.common.dao.dto.condition.BaseCondition;
 import io.github.xfdzcoder.noj.cloud.manage.question.entity.Answer;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
  * @author xfdzcoder
  */
 
+@Data
 @EqualsAndHashCode(callSuper = true)
 public class AnswerCondition extends BaseCondition<Answer> {
 
+    private Long questionInfoId;
+
+    @Override
+    public LambdaQueryWrapper<Answer> getLambdaQueryWrapper() {
+        return super.getLambdaQueryWrapper()
+                .eq(Answer::getQuestionInfoId, questionInfoId);
+    }
 }
